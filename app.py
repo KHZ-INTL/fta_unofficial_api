e408e6ca287c9889bdaa476a1d89f2fdd99715b4
khz13 <khz.intl@gmail.com>
2018-07-07 01:06:12 +0930

    fix return type issues

--- app.py
+++ app.py
@@ -1,134 +1,118 @@
-#!/bin/python3
-
-"""Unofficial FTA API for fetching flight schedules."""
-
-import datetime
-import os
-import requests
+from flask import Flask, jsonify
 from bs4 import BeautifulSoup
-from flask import Flask, jsonify, request
+import requests
+import datetime
+
+
+# Init flask application
+app = Flask(__name__)
 
 
 class fta():
 
     def __init__(self, alias):
-        self.alias = alias
+        self.alias = str(alias)
 
-    def get(self):
-        last_return = self.request_data()
-        last_return = self.parse_data(last_return)
+    def http_get(self):
+        """
+        HTTP GET page 
+        """
+        user_id = {'rdUsername': self.alias,
+                   'rdFormLogon': 'True',
+                   'rdPassword': self.alias,
+                   'Submit1': 'Logon'}
+        response = requests.post('http://202.158.223.244/OPS_STUDENT_REPORTS/rdPage.aspx', data=user_id)
+
+        if response.status_code != 200:
+            return "ERROR: http_get -  {}".format(response.status_code)
+
+        return response.text
+    
+    def format_date(self, date, method):
+        """
+        Format date/time to and from iso standard and %d/%m/%YT:%H:%M:%S
+        """
         
-        return last_return
-
-    def format_date(self, data, method):
-        """Format date/time string to and from iso standard
-        This function is mainly used to format date before inserting into to the data base
-        Arguments:
-            data {string} -- [date/time string]
-            method {string} -- [the value of string is used to decide wether convert data (date/time string) into iso or %d/%m/%YT:%H:%M:%S]
-        Returns:
-            date -- dateTime object"""
-
         if method == "iso":
             date = datetime.datetime.strptime(data, "%d/%m/%y %H:%M").isoformat()
-            return date
+        
         elif method == "display":
             date = datetime.datetime.strptime(data, "%Y-%m-%dT%H:%M:%S")
-        
-        return date
+
+        return date 
+ 
 
 
-    def request_data(self):
-        """Get raw data from FTA online schedules portal website using Requests
-        Alias variable is used as login credential when requesting student schedules
-        Returns:
-            raw_schedule -- return html data from get request
+    def parse_html(self, html):
         """
-        user_id = {
-            'rdUsername': str(self.alias),
-            'rdFormLogon': 'True',
-            'rdPassword': str(self.alias),
-            'Submit1': 'Logon'}
-        raw_schedule = requests.post(
-            'http://202.158.223.244/OPS_STUDENT_REPORTS/rdPage.aspx', data=user_id)
-
-        if raw_schedule.status_code != 200:
-            return raw_schedule.status_code
-        
-        return raw_schedule
-
-
-    def parse_data(self, data):
-
-        """Parse raw html data (data) using BeautifulSoup into a dictionary
-        Arguments:
-            data {html} -- the result of GET request for student schedules
-        Returns:
-            in_vars -- dictionary which contains schedule information
+        Parses the table data from html into dictionary
         """
-
         day = ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATUARDAY", "SUNDAY"]
         rem = ["Time", "Captain", "Crew", "Aircraft", "Module", "Exercise", "Description", "Fly Type"]
-        in_vars = {"DATE": "", "CAPITAN": "", "CREW": "", "AIRCRAFT": "", "MODULE": "", "EXCERCISE": "", "DESCRIPTION": "", "FLY_TYPE": ""}
-        row = []
-        rowb = []
-        index = []
+        schedules_dictionary= {"DATE": "", "CAPITAN": "", "CREW": "", "AIRCRAFT": "", "MODULE": "", "EXCERCISE": "", "DESCRIPTION": "", "FLY_TYPE": ""}
 
-        soup = BeautifulSoup(data.text, 'html.parser')
+        
+        table_data = []
+        table_data_without_header = []
+        day_index = []
+        
+        if len(html) == 0:
+            return "ERROR: parse_html - len(html) = 0"
+        
+        soup = BeautifulSoup(html, 'lxml')
         table_rows = soup.find("table").find_all("tr")
 
+        # Extract table data from table 
         for tr in table_rows:
-            td = tr.find_all('td')
-            [row.append(i.text) for i in td]
-
-        for i in row:
+            td = tr.find_all("td")
+            for i in td:
+                table_data.app(i.text)
+        
+        # Remove table headers
+        for i in table_data:
             if i in rem or i == "":
                 continue
             else:
-                rowb.append(i)
+                table_data_without_header.append(i)
 
-        for i in rowb:
+        # Extract index of date
+        for td in table_data_without_header:
             for d in day:
-                if d in str(i):
-                    index.append((rowb.index(i)))
+                if d in str(td):
+                    day_index.append((table_data_without_header.index(td)))
+       
+       # Populate schedules_dictionary The return dictionary
+        for a in day_index:
+            date_time = table_data_without_header[a].split("-", 2)[1].replace(" ", "")
+            date_time = date_time + " " + table_data_without_header[a + 1]
+            schedules_dictionary["DATE"] = str(self.format_date(date_time, "iso"))
 
-        for a in index:
-            date_time = rowb[a].split("-", 2)[1].replace(" ", "")
-            date_time = date_time + " " + rowb[a + 1]
-            in_vars["DATE"] = str(self.format_date(date_time, "iso"))
+            if ":" in table_data_without_header[(a + 1)]:
+                schedules_dictionary["CAPITAN"] = table_data_without_header[(a + 2)]
+                schedules_dictionary["CREW"] = table_data_without_header[(a + 3)]
+                schedules_dictionary["AIRCRAFT"] = table_data_without_header[(a + 4)]
+                schedules_dictionary["MODULE"] = table_data_without_header[(a + 5)]
+                schedules_dictionary["EXCERCISE"] = table_data_without_header[(a + 6)]
+                schedules_dictionary["DESCRIPTION"] = table_data_without_header[(a + 7)]
+                schedules_dictionary["FLY_TYPE"] = table_data_without_header[(a + 8)]
 
-            if ":" in rowb[(a + 1)]:
-                in_vars["CAPITAN"] = rowb[(a + 2)]
-                in_vars["CREW"] = rowb[(a + 3)]
-                in_vars["AIRCRAFT"] = rowb[(a + 4)]
-                in_vars["MODULE"] = rowb[(a + 5)]
-                in_vars["EXCERCISE"] = rowb[(a + 6)]
-                in_vars["DESCRIPTION"] = rowb[(a + 7)]
-                in_vars["FLY_TYPE"] = rowb[(a + 8)]
+        return jsonify({'Return': schedules_dictionary})
+    
 
-        print("\n\n")
+def hi(name):
+    a = fta(name)
+    if len(name) < 4:
+        return jsonify({"ERROR": "Invalid Alias: length 4"})
 
-        print("{}\n\n".format(in_vars))
+    last_retun = a.http_get()
+    last_retun = a.parse_html(last_retun)
+    return last_retun 
 
-
-        return jsonify({'retunr': in_vars})
-
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def ret_none():
-    return "HI"
-
-@app.route('/api/<string:alias>', methods=['GET'])
-def get_schedules(alias):
-    schedules = ""
-    if len(alias) > 4:
-        schedules = fta(alias)
-        schedules.get()
-    else:
-        schedules = "alias not > 4"
-    return schedules
+@app.route("/<string:alias>")
+def hello(alias):
+    return hi(alias) 
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
+    
+
